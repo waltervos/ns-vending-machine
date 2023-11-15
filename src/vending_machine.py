fc5bcf7b324ad21bd63b7292235e089d542d9479
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-11-15 13:27:09 +0000

    Changes after demo

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -1,2 +1,21 @@
+from enum import Enum
+  
+class Choice(Enum):
+  COKE = "Coke choice"
+  FIZZY_ORANGE = "Fizzy orange choice"
+  BEER = "Beer choice"
+  
+class Can(Enum):
+  NOTHING = "no can"
+  COLA = "Can of Cola"
+  FANTA = "Can of Fanta"
+  
 class VendingMachine:
-  pass
+  def __init__(self):
+    self._choice_can_map = {}
+    
+  def deliver(self, choice):
+    return self._choice_can_map[choice] if choice in self._choice_can_map else Can.NOTHING
+  
+  def configure(self, choice, can):
+    self._choice_can_map[choice] = can
--- test/vending_machine_test.py
+++ test/vending_machine_test.py
@@ -1,8 +1,23 @@
 from hamcrest import assert_that, equal_to
+from vending_machine import VendingMachine, Choice, Can
+import pytest
 
 
 class TestVendingMachine:
+  
+  @pytest.fixture()
+  def vending_machine(self):
+    vending_machine = VendingMachine()
+    vending_machine.configure(Choice.FIZZY_ORANGE, Can.FANTA) 
+    vending_machine.configure(Choice.COKE, Can.COLA) 
+    return vending_machine
 
-  def test_a_new_VendingMachine(self):
-       assert_that(True, equal_to(False))
-       
+  def test_given_a_vending_machine_when_unconfigured_then_does_not_deliver_can_of_choice(self, vending_machine):
+    assert_that(vending_machine.deliver(Choice.BEER), equal_to(Can.NOTHING))
+
+  def test_vending_machine_delivers_can_of_cola_for_coke_choice(self, vending_machine):
+    assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.COLA))
+    
+  def test_vending_machine_delivers_can_of_fanta_for_fizzy_orange_choice(self, vending_machine):
+    assert_that(vending_machine.deliver(Choice.FIZZY_ORANGE), equal_to(Can.FANTA))
+    
