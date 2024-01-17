8eb4ecdacec39c9bdcb735d0934aa4863bac18a6
Walter Vos <walter.vos@ns.nl>
2024-01-17 15:24:28 +0100

    Refactored feature envy into circular dependency
    Co-authored-by: San Giorgi, Renee <renee.sangiorgi@ns.nl>
    Co-authored-by: Rogier van Dinther <rogierdinther@users.noreply.github.com>
    Co-authored-by: Heefer, Hugo HCH <Hugo.Heefer@NS.NL>
    Co-authored-by: nsr-bhu <nsr-bhu@users.noreply.github.com>
    Co-authored-by: Waard, Maarten de <maarten.dewaard@ns.nl>
    Co-authored-by: Hendrikse, Zeger <zeger.hendrikse@ns.nl>

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -1,38 +1,59 @@
+from dataclasses import dataclass
 from enum import Enum
+from typing import Any
 
 
-class Choice(Enum):
-    COKE = "Coke choice"
+class Button(Enum):
+    COLA = "Cola choice"
     FIZZY_ORANGE = "Fizzy orange choice"
     BEER = "Beer choice"
 
 
 class Can(Enum):
-    NOTHING = "no can"
-    COLA = "Can of Cola"
+    NOTHING = "No can"
+    COKE = "Can of Coke"
     FANTA = "Can of Fanta"
 
 
+@dataclass
+class Drawer:
+    can_type: Can
+    price_in_cents: int
+    vending_machine: "VendingMachine" = None
+
+    def _pay_for_can(self):
+        self.vending_machine._balance_in_cents -= self.price_in_cents
+
+    def _can_afford_can(self):
+        return self.price_in_cents <= self.vending_machine._balance_in_cents
+    
+    def drop_can(self):
+        if self._can_afford_can():
+            self._pay_for_can()
+            return self.can_type
+        return Can.NOTHING
+        
+
 class VendingMachine:
     def __init__(self):
-        self._choice_can_map = {}
-        self._choice_price_map = {}
+        self._choice_can_map: dict[Button, Drawer] = {}
         self._balance_in_cents = 0
-
+        
     def deliver(self, choice):
-        if not choice in self._choice_can_map:
-            return Can.NOTHING
-
-        price_in_cents = self._choice_price_map[choice]
-        if price_in_cents > self._balance_in_cents:
-            return Can.NOTHING
-
-        self._balance_in_cents -= price_in_cents
-        return self._choice_can_map[choice]
+        if self._available(choice):
+            drawer = self._choice_can_map[choice]
+            return drawer.drop_can()
+        return Can.NOTHING
 
     def insert(self, amount_in_cents):
         self._balance_in_cents = amount_in_cents
 
-    def configure(self, choice, can, price_in_cents=0):
-        self._choice_can_map[choice] = can
-        self._choice_price_map[choice] = price_in_cents
+    def configure(self, choice, can_type, price_in_cents=0):
+        self._choice_can_map[choice] = Drawer(can_type, price_in_cents, self)
+
+    def _available(self, choice):
+        return choice in self._choice_can_map
+    
+    
+    
+    
--- test/vending_machine_test.py
+++ test/vending_machine_test.py
@@ -1,6 +1,6 @@
 import pytest
 from hamcrest import assert_that, equal_to
-from vending_machine import Can, Choice, VendingMachine
+from src.vending_machine import Can, Button, VendingMachine
 
 
 class TestFreeDrinksVendingMachine:
@@ -8,18 +8,18 @@ class TestFreeDrinksVendingMachine:
     @pytest.fixture()
     def vending_machine(self):
         vending_machine = VendingMachine()
-        vending_machine.configure(Choice.FIZZY_ORANGE, Can.FANTA)
-        vending_machine.configure(Choice.COKE, Can.COLA)
+        vending_machine.configure(Button.FIZZY_ORANGE, Can.FANTA)
+        vending_machine.configure(Button.COLA, Can.COKE)
         return vending_machine
 
     def test_given_a_vending_machine_when_unconfigured_then_does_not_deliver_can_of_choice(self, vending_machine):
-        assert_that(vending_machine.deliver(Choice.BEER), equal_to(Can.NOTHING))
+        assert_that(vending_machine.deliver(Button.BEER), equal_to(Can.NOTHING))
 
     def test_vending_machine_delivers_can_of_cola_for_coke_choice(self, vending_machine):
-        assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.COLA))
+        assert_that(vending_machine.deliver(Button.COLA), equal_to(Can.COKE))
 
     def test_vending_machine_delivers_can_of_fanta_for_fizzy_orange_choice(self, vending_machine):
-        assert_that(vending_machine.deliver(Choice.FIZZY_ORANGE), equal_to(Can.FANTA))
+        assert_that(vending_machine.deliver(Button.FIZZY_ORANGE), equal_to(Can.FANTA))
 
 
 class TestPaidDrinksVendingMachine:
@@ -27,26 +27,26 @@ class TestPaidDrinksVendingMachine:
     @pytest.fixture()
     def vending_machine(self):
         vending_machine = VendingMachine()
-        vending_machine.configure(Choice.FIZZY_ORANGE, Can.FANTA, 400)
-        vending_machine.configure(Choice.COKE, Can.COLA, 500)
+        vending_machine.configure(Button.FIZZY_ORANGE, Can.FANTA, 400)
+        vending_machine.configure(Button.COLA, Can.COKE, 500)
         return vending_machine
 
     def test_vending_machine_delivers_no_can_of_cola_for_unpaid_coke_choice(self, vending_machine):
-        assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.NOTHING))
+        assert_that(vending_machine.deliver(Button.COLA), equal_to(Can.NOTHING))
 
     def test_vending_machine_delivers_can_of_cola_for_paid_coke_choice(self, vending_machine):
         vending_machine.insert(500)
-        assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.COLA))
+        assert_that(vending_machine.deliver(Button.COLA), equal_to(Can.COKE))
 
     def test_vending_machine_delivers_can_of_cola_for_too_much_paid_coke_choice(self, vending_machine):
         vending_machine.insert(600)
-        assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.COLA))
+        assert_that(vending_machine.deliver(Button.COLA), equal_to(Can.COKE))
 
     def test_vending_machine_delivers_can_of_fanta_for_paid_fizzy_orange_choice(self, vending_machine):
         vending_machine.insert(400)
-        assert_that(vending_machine.deliver(Choice.FIZZY_ORANGE), equal_to(Can.FANTA))
+        assert_that(vending_machine.deliver(Button.FIZZY_ORANGE), equal_to(Can.FANTA))
 
     def test_vending_machine_delivers_nothing_for_second_paid_coke_choice(self, vending_machine):
         vending_machine.insert(500)
-        _ = vending_machine.deliver(Choice.COKE)
-        assert_that(vending_machine.deliver(Choice.COKE), equal_to(Can.NOTHING))
+        _ = vending_machine.deliver(Button.COLA)
+        assert_that(vending_machine.deliver(Button.COLA), equal_to(Can.NOTHING))
